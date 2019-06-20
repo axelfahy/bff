1f214a7a85af0fd5c66aa5b9783ac6206087ea28
Axel Fahy <axel.fahy@heig-vd.ch>
2019-06-20 17:21:00 +0200

    chore(init): add `__all__` variable in `__init__.py` file
    Add docstring at begining of file as well.

--- bff/__init__.py
+++ bff/__init__.py
@@ -1,8 +1,25 @@
-# __init__.py
-from .fancy import (concat_with_categories, get_peaks, parse_date,
-                    plot_history, plot_predictions, plot_series,
-                    plot_true_vs_pred, read_sql_by_chunks,
-                    sliding_window, value_2_list)
+"""
+All of bff' functions.
+"""
+from .fancy import (
+    concat_with_categories, get_peaks, parse_date, plot_history,
+    plot_predictions, plot_series, plot_true_vs_pred, read_sql_by_chunks,
+    sliding_window, value_2_list
+)
+
+# Public object of the module.
+__all__ = [
+    'concat_with_categories',
+    'get_peaks',
+    'parse_date',
+    'plot_history',
+    'plot_predictions',
+    'plot_series',
+    'plot_true_vs_pred',
+    'read_sql_by_chunks',
+    'sliding_window',
+    'value_2_list',
+]
 
 # Version of the bff package.
 __version__ = '0.1.2'
