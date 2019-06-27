2662f146d9fe10cccfbbbcf0525d412be32cc44b
Axel Fahy <axel@fahy.net>
2019-06-28 01:15:58 +0200

    fix(doc): remove extension for Read the Docs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -60,7 +60,7 @@ html_theme = 'sphinx_rtd_theme'     # Read the Docs theme.
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 # Theme from Read the Docs.
 html_theme_options = {
