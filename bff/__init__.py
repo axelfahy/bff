3866cada85ddb95994b72e96b82544d4b1b4ab5d
Axel Fahy <axel@fahy.net>
2019-07-02 23:20:31 +0200

    feat(config): add class `FancyConfig` to handle configurations

--- bff/__init__.py
+++ bff/__init__.py
@@ -1,9 +1,15 @@
 """All of bff' functions."""
+import logging
+
+from ._version import get_versions
+
 from .fancy import (
     concat_with_categories, get_peaks, idict, mem_usage_pd, parse_date,
     plot_history, plot_predictions, plot_series, plot_true_vs_pred,
     read_sql_by_chunks, sliding_window, value_2_list
 )
+
+from .config import FancyConfig
 
 # Public object of the module.
 __all__ = [
@@ -19,8 +25,12 @@ __all__ = [
     'read_sql_by_chunks',
     'sliding_window',
     'value_2_list',
+    'FancyConfig',
 ]
 
-from ._version import get_versions
+# Logging configuration.
+FORMAT = '%(asctime)-15s %(message)s'
+logging.basicConfig(format=FORMAT)
+
 __version__ = get_versions()['version']
 del get_versions
